1a08b2664920f44d7f3f85d680e0203a92a0c411
Nouu <noelarrieulacoste@yahoo.fr>
2020-12-26 19:23:19 +0100

    Put type on rental vector and enumeration of this vector

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -3,7 +3,7 @@ import java.util.Enumeration;
 
 public class Customer {
     private String name;
-    private Vector rentals = new Vector();
+    private Vector<Rental> rentals = new Vector<>();
 
     public Customer(String name) {
         this.name = name;
@@ -20,7 +20,7 @@ public class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration rentals = this.rentals.elements();
+        Enumeration<Rental> rentals = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
 
         while (rentals.hasMoreElements()) {
