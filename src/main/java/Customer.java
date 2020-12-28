07110ab37d982bb148935720a9bcb53c37e94ab7
Nouu <noelarrieulacoste@yahoo.fr>
2020-12-28 20:06:05 +0100

    refactor Customer to handle CustomerStatement class

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -4,6 +4,7 @@ import java.util.Enumeration;
 public class Customer {
     private final String name;
     private final Vector<Rental> rentals = new Vector<>();
+    private final CustomerStatement customerStatement = new CustomerStatement(rentals);
 
     public Customer(String name) {
         this.name = name;
@@ -18,54 +19,6 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Enumeration<Rental> rentals = this.rentals.elements();
-        StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
-
-        while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
-            Rental each = rentals.nextElement();
-
-            // determines the amount for each line
-            switch (each.getMovie().getPriceCode()) {
-                case REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-            frequentRenterPoints++;
-
-            if (each.getMovie().getPriceCode().equals(MovieType.NEW_RELEASE)
-                    && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-
-            result.append("\t");
-            result.append(each.getMovie().getTitle());
-            result.append("\t");
-            result.append(thisAmount);
-            result.append("\n");
-            totalAmount += thisAmount;
-        }
-
-        result.append("You owed ");
-        result.append(totalAmount);
-        result.append("\n");
-        result.append("You earned ");
-        result.append(frequentRenterPoints);
-        result.append(" frequent renter points\n");
-
-
-        return result.toString();
+        return customerStatement.statement(getName());
     }
 }
